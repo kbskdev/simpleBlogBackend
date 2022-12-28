fe177cf40ed91aa4e7d546b9fb1928e216696deb
kbskdev <milosz.olsztyn@gmail.com>
2022-12-28 12:50:30 +0100

    fuckign collection name

--- deleteComments.js
+++ deleteComments.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 const Model = require("./models/commentsModel")
 
 dotenv.config({path:"./config.env"})
+
 mongoose.connect(`${process.env.database}`,{useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false}).then(con=>{
     console.log("polaczono")
 }).catch(()=>{console.log("nie udalo sie")})
@@ -11,6 +12,6 @@ mongoose.connect(`${process.env.database}`,{useNewUrlParser:true, useUnifiedTopo
 const post = process.argv[2]
 const commsNum = process.argv[3]
 
-Model.Posts.findByIdAndUpdate({_id:post},{$set:{comments:[]}}).then(data=>{
+Model.Posts.findByIdAndUpdate({_id:"638e7fa64baa1421bc9aaeea"},{$set:{comments:[]}}).then(data=>{
     console.log(data)
 })
